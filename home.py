1be0e53330a9e7a01e25222944472a887075540d
mingnatthakitt <159937632+mingnatthakitt@users.noreply.github.com>
2024-08-21 00:28:20 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,27 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-#/* font no work sadge*/
-#css = """
-#@import url('https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@500&display=swap');
-#body  {
-#    font-family: 'Rubik', sans-serif;
-#    font-optical-sizing: auto;
-#    font-style: normal;
-#}
-#"""
-#st.markdown(f'<style>{css}</style>', unsafe_allow_html=True )
-#https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@500&display=swap
-#https://fonts.googleapis.com/css2?family=Rubik
-# Inject custom CSS and JavaScript for fade-in and slide-in animations
-#components.html("<link href='https://fonts.googleapis.com/css2?family=Rubik' rel='stylesheet'>")
 
 def show():
 
-    #doesn't work nav_bar css styling covered it
-    #st.logo(
-    #    image="images/TumorVision.jpg",
-    #)
     st.write("#")
 
     col1, col2, col3 = st.columns([1, 4, 1])
@@ -29,7 +11,7 @@ def show():
         st.write("")
 
     with col2:
-        st.markdown("<p style='text-align: right; font-size: 50px; font-weight:bold;'>Welcome to <span style= color:#3c6ca8; font-style:italic;>TumorVision</span></p>", unsafe_allow_html=True)
+        st.markdown("<p style='text-align: right; font-size: 48px; font-weight:bold;'>Welcome to <span style= color:#3c6ca8; font-style:italic;>TumorVision</span></p>", unsafe_allow_html=True)
 
         col4, col5, col6 = st.columns([2,3,2])
 
@@ -43,12 +25,11 @@ def show():
             st.write("")
 
 
-        #st.markdown(f"""<h2 style='text-align: left; font-size: 18px; font-weight:bold; font: Rubik; >
         st.markdown("""
         <style>
             .custom-text {
-                line-height: 1.6; 
-                font-size: 20px;  
+                line-height: 1.5; 
+                font-size: 19.5px;  
             }
             .custom-text span {
                 color: #3c6ca8;
@@ -69,32 +50,35 @@ def show():
             }
             .custom-text mission {
                 font-size: 30px;
-            }
+            }d
             .custom-text redirect {
                 font-size: 22px;
+            }
+            .small-text {
+                line-height: 1.6;
+                font-size: 17.2px;   
             }
         </style>
         <p class="custom-text">
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; At <span>TumorVision</span>, we are pioneering the future of medical diagnostics. Our innovative initiative harnesses the power of cutting-edge machine learning models to support healthcare professionals in the critical task of brain tumor diagnosis. By bridging the gap between artificial intelligence and medical expertise, we're setting new standards in early detection and patient care.
         </p>
         <br>
-        
+
         """, unsafe_allow_html=True)
         st.markdown("""
-        <p class="custom-text"><redirect><strong>
-        To try out our service please go the the dedicated <span>TumorVision</span> page
-        </strong></redirect></p>
+        <p style='text-align: left; font-size: 25px; font-weight:bold;'>
+        To try out our service please go the the dedicated <span style= color:#3c6ca8; >TumorVision </span> page
+        </p><br><br>
         """, unsafe_allow_html=True)
-        st.write("#")
 
-        # Insert the SVG image here using st.image
+
         col10, col11,= st.columns([0.55, 0.45])
         with col10:
             st.markdown("""
             <p class="custom-text"> 
                 <mission><blue>Our Mission</blue></mission>
                 <br>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; TumorVision is more than just an AI tool - it's a lifeline in the fight against terminal illnesses. Our mission is twofold:
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; TumorVision is more than just an AI tool - it's a lifeline in the fight against terminal illnesses. Our mission have twofolds:
                 <br>         
             </p>
             <p class="custom-text">
@@ -105,7 +89,7 @@ def show():
                 <br>
                 <strong>2) Empower Healthcare Professionals</strong>: 
                 <br>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We equip medical experts with state-of-the-art technology, allowing them to make informed decisions with unprecedented speed and precision.
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We equip medical experts with state-of-the-art technology, allowing them to make informed decisions with unprecedented speed and precision. 
                 <br>
                 Our system serves as a crucial first step in the treatment process, potentially alleviating the impact of disease through early intervention and tailored care plans.    
             </p>
@@ -116,8 +100,7 @@ def show():
             st.image("assets/model3d.jpg")
 
 
-        
-        #st.button("Try Our App",type="primary",on_click= switch_page('TumorVision')) //this no work
+
 
         st.markdown("<p style='text-align: center; font-size: 30px; font-weight:bold; text-decoration: underline;'>Key Features</p>", unsafe_allow_html=True)
 
@@ -128,7 +111,7 @@ def show():
             justify-content: space-evenly;
             gap: 0px;
         }
-          
+
         .custom-box {
             border: 2px solid #3c6ca8;
             border-radius: 12px;
@@ -176,14 +159,15 @@ def show():
                     </div>
                 </div>
             """, unsafe_allow_html=True)
-    
+
         st.markdown("""
         <br>
         <br>
         <p class="custom-text">
         <mission>Why Choose <span>TumorVision?</span></mission>
         <br>
-        <br>
+        </p>
+        <p class="small-text">
         &nbsp;&nbsp;&nbsp;&nbsp;&#x2022; <strong>Cutting-Edge Technology :</strong> Our AI models are built on the latest advancements in machine learning and medical imaging.
         <br>
         &nbsp;&nbsp;&nbsp;&nbsp;&#x2022; <strong>Time Efficiency :</strong> Rapid diagnosis allows for quicker treatment initiation, potentially saving crucial time in critical cases.
@@ -191,12 +175,8 @@ def show():
         &nbsp;&nbsp;&nbsp;&nbsp;&#x2022; <strong>Support for Medical Professionals :</strong> We augment the expertise of healthcare providers, offering an additional layer of diagnostic confidence.         
         <br>
         &nbsp;&nbsp;&nbsp;&nbsp;&#x2022; <strong>Ongoing Research and Development :</strong> Our commitment to innovation means we're constantly improving our model and expanding our capabilities.         
-        </p>
-        """, unsafe_allow_html=True)
+        </p> """, unsafe_allow_html=True)
 
-        #st.write("""Stay safe and informed with TumorVision!""")
-
-        #st.info("This is the Home page demo. You can customize this content with more information about your project.")
 
     with col3:
         st.write("")
